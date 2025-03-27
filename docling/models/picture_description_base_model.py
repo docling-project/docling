ff54e446c6f374c7886d18ae4510dcf3cbff0734
Rowan Skewes <rowan.skewes@gmail.com>
2025-03-27 17:08:55 +1100

    fix: Implement PictureDescriptionApiOptions.bitmap_area_threshold
    Signed-off-by: Rowan Skewes <rowan.skewes@gmail.com>

--- docling/models/picture_description_base_model.py
+++ docling/models/picture_description_base_model.py
@@ -63,8 +63,19 @@ class PictureDescriptionBaseModel(
         elements: List[PictureItem] = []
         for el in element_batch:
             assert isinstance(el.item, PictureItem)
-            elements.append(el.item)
-            images.append(el.image)
+            describe_image = True
+            # Don't describe the image if it's smaller than the threshold
+            if len(el.item.prov) > 0:
+                prov = el.item.prov[0]  # PictureItems have at most a single provenance
+                page = doc.pages.get(prov.page_no)
+                if page is not None:
+                    page_area = page.size.width * page.size.height
+                    area_fraction = prov.bbox.area() / page_area
+                    if area_fraction < self.options.bitmap_area_threshold:
+                        describe_image = False
+            if describe_image:
+                elements.append(el.item)
+                images.append(el.image)
 
         outputs = self._annotate_images(images)
 
