baca679beb787cd135c1e9a42323d1cae3b510f4
Clément Doumouro <clement.doumouro@gmail.com>
2025-04-08 17:37:58 +0200

    chore(ocr): revert layout updates
    Signed-off-by: Clément Doumouro <clement.doumouro@gmail.com>

--- docling/utils/orientation.py
+++ docling/utils/orientation.py
@@ -1,22 +1,9 @@
-from collections import Counter
-from operator import itemgetter
 from typing import Tuple
 
 from docling_core.types.doc import BoundingBox, CoordOrigin
-from docling_core.types.doc.page import BoundingRectangle, TextCell
+from docling_core.types.doc.page import BoundingRectangle
 
 CLIPPED_ORIENTATIONS = [0, 90, 180, 270]
-
-
-def _clipped_orientation(angle: float) -> int:
-    return min((abs(angle - o) % 360, o) for o in CLIPPED_ORIENTATIONS)[1]
-
-
-def detect_orientation(cells: list[TextCell]) -> int:
-    if not cells:
-        return 0
-    orientation_counter = Counter(_clipped_orientation(c.rect.angle_360) for c in cells)
-    return max(orientation_counter.items(), key=itemgetter(1))[0]
 
 
 def rotate_bounding_box(
