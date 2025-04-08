d8873aa0c97d44c4565d44e20f113f289ad32bd6
Benichou <fbenichou@deloitte.ca>
2025-04-08 01:00:12 -0400

    fix/adding a commit with a signature Signed-off-by: Franck Benichou franck.benichou@sciencespo.fr
    Signed-off-by: Benichou <fbenichou@deloitte.ca>

--- docling/backend/mspowerpoint_backend.py
+++ docling/backend/mspowerpoint_backend.py
@@ -406,7 +406,7 @@ class MsPowerpointDocumentBackend(DeclarativeDocumentBackend, PaginatedDocumentB
                 if shape.shape_type == MSO_SHAPE_TYPE.PICTURE:
                     # Handle Pictures
                     drawing_blip = self.xpath_expr(shape.element)
-                    if drawing_blip:
+                    if drawing_blip: #ensure there is a drwaing blip
                         self.handle_pictures(shape, parent_slide, slide_ind, doc, drawing_blip, slide)
                 # If shape doesn't have any text, move on to the next shape
                 if not hasattr(shape, "text"):
