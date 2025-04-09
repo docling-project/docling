ad1541e8cf18415dc556f4211341c299c81043b4
Gabe Goodhart <ghart@us.ibm.com>
2025-04-09 08:02:01 -0600

    refactor: Move OpenAI API call logic into utils.utils
    This will allow reuse of this logic in a generic VLM model
    
    NOTE: There is a subtle change here in the ordering of the text prompt and
    the image in the call to the OpenAI API. When run against Ollama, this
    ordering makes a big difference. If the prompt comes before the image, the
    result is terse and not usable whereas the prompt coming after the image
    works as expected and matches the non-OpenAI chat API.
    
    Branch: OllamaVlmModel
    
    Signed-off-by: Gabe Goodhart <ghart@us.ibm.com>

--- docling/utils/utils.py
+++ docling/utils/utils.py
@@ -1,11 +1,18 @@
+import base64
 import hashlib
+import logging
 from io import BytesIO
 from itertools import islice
 from pathlib import Path
 from typing import List, Union
 
 import requests
+from PIL import Image
 from tqdm import tqdm
+
+from docling.datamodel.base_models import OpenAiApiResponse
+
+_log = logging.getLogger(__name__)
 
 
 def chunkify(iterator, chunk_size):
@@ -63,3 +70,57 @@ def download_url_with_progress(url: str, progress: bool = False) -> BytesIO:
 
     buf.seek(0)
     return buf
+
+
+def openai_image_request(
+    image: Image.Image,
+    prompt: str,
+    url: str = "http://localhost:11434/v1/chat/completions",  # Default to ollama
+    apikey: str | None = None,
+    timeout: float = 20,
+    headers: dict[str, str] | None = None,
+    **params,
+) -> str:
+    img_io = BytesIO()
+    image.save(img_io, "PNG")
+    image_base64 = base64.b64encode(img_io.getvalue()).decode("utf-8")
+    messages = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/png;base64,{image_base64}"
+                    },
+                },
+                {
+                    "type": "text",
+                    "text": prompt,
+                },
+            ],
+        }
+    ]
+
+    payload = {
+        "messages": messages,
+        **params,
+    }
+
+    headers = headers or {}
+    if apikey is not None:
+        headers["Authorization"] = f"Bearer {apikey}"
+
+    r = requests.post(
+        str(url),
+        headers=headers,
+        json=payload,
+        timeout=timeout,
+    )
+    if not r.ok:
+        _log.error(f"Error calling the API. Response was {r.text}")
+    r.raise_for_status()
+
+    api_resp = OpenAiApiResponse.model_validate_json(r.text)
+    generated_text = api_resp.choices[0].message.content.strip()
+    return generated_text
