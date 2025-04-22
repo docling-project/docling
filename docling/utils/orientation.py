4c6db7401250a351de3f3aeb54b662da0ed21224
Clément Doumouro <clement.doumouro@gmail.com>
2025-04-22 17:35:43 +0200

    chore(ocr): revert layout updates
    Signed-off-by: Clément Doumouro <clement.doumouro@gmail.com>

--- docling/utils/orientation.py
+++ docling/utils/orientation.py
@@ -13,44 +13,44 @@ def rotate_bounding_box(
     # Bounding rectangle start with r_0 at the bottom left whatever the
     # coordinate system. Then other corners are found rotating counterclockwise
     bbox = bbox.to_top_left_origin(im_size[1])
-    l, t, w, h = bbox.l, bbox.t, bbox.width, bbox.height
+    left, top, width, height = bbox.l, bbox.t, bbox.width, bbox.height
     im_h, im_w = im_size
     angle = angle % 360
     if angle == 0:
-        r_x0 = l
-        r_y0 = t + h
-        r_x1 = r_x0 + w
+        r_x0 = left
+        r_y0 = top + height
+        r_x1 = r_x0 + width
         r_y1 = r_y0
-        r_x2 = r_x0 + w
-        r_y2 = r_y0 - h
+        r_x2 = r_x0 + width
+        r_y2 = r_y0 - height
         r_x3 = r_x0
-        r_y3 = r_y0 - h
+        r_y3 = r_y0 - height
     elif angle == 90:
-        r_x0 = im_w - (t + h)
-        r_y0 = l
+        r_x0 = im_w - (top + height)
+        r_y0 = left
         r_x1 = r_x0
-        r_y1 = r_y0 + w
-        r_x2 = r_x0 + h
-        r_y2 = r_y0 + w
+        r_y1 = r_y0 + width
+        r_x2 = r_x0 + height
+        r_y2 = r_y0 + width
         r_x3 = r_x0
-        r_y3 = r_y0 + w
+        r_y3 = r_y0 + width
     elif angle == 180:
-        r_x0 = im_h - l
-        r_y0 = im_w - (t + h)
-        r_x1 = r_x0 - w
+        r_x0 = im_h - left
+        r_y0 = im_w - (top + height)
+        r_x1 = r_x0 - width
         r_y1 = r_y0
-        r_x2 = r_x0 - w
-        r_y2 = r_y0 + h
+        r_x2 = r_x0 - width
+        r_y2 = r_y0 + height
         r_x3 = r_x0
-        r_y3 = r_y0 + h
+        r_y3 = r_y0 + height
     elif angle == 270:
-        r_x0 = t + h
-        r_y0 = im_h - l
+        r_x0 = top + height
+        r_y0 = im_h - left
         r_x1 = r_x0
-        r_y1 = r_y0 - w
-        r_x2 = r_x0 - h
-        r_y2 = r_y0 - w
-        r_x3 = r_x0 - h
+        r_y1 = r_y0 - width
+        r_x2 = r_x0 - height
+        r_y2 = r_y0 - width
+        r_x3 = r_x0 - height
         r_y3 = r_y0
     else:
         msg = (
