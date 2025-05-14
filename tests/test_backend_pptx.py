7468137c55be93d3de9f015b99a5849a9c1ffd97
Benichou <fbenichou@deloitte.ca>
2025-05-13 20:46:08 -0400

    fix/removed generate=True in test_backend_pptx.py in verify_export method to not conflict with main branch Signed-off-by: Franck Benichou franck.benichou@sciencespo.fr
    Signed-off-by: Benichou <fbenichou@deloitte.ca>

--- tests/test_backend_pptx.py
+++ tests/test_backend_pptx.py
@@ -45,13 +45,13 @@ def test_e2e_pptx_conversions():
         doc: DoclingDocument = conv_result.document
 
         pred_md: str = doc.export_to_markdown()
-        assert verify_export(pred_md, str(gt_path) + ".md", generate=True), "export to md"
+        assert verify_export(pred_md, str(gt_path) + ".md"), "export to md"
 
         pred_itxt: str = doc._export_to_indented_text(
             max_text_len=70, explicit_tables=False
         )
         assert verify_export(
-            pred_itxt, str(gt_path) + ".itxt", generate=True
+            pred_itxt, str(gt_path) + ".itxt"
         ), "export to indented-text"
 
         assert verify_document(
