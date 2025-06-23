638820d889c2fc19eee3d355765451028496fcd7
Peter Staar <taa@zurich.ibm.com>
2025-06-23 18:20:03 +0200

    reformatting
    Signed-off-by: Peter Staar <taa@zurich.ibm.com>

--- docs/examples/minimal_asr_pipeline.py
+++ docs/examples/minimal_asr_pipeline.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     print(doc.export_to_markdown())
 
     # Expected output:
-    # 
+    #
     # [time: 0.0-4.0]  Shakespeare on Scenery by Oscar Wilde
-    # 
+    #
     # [time: 5.28-9.96]  This is a LibriVox recording. All LibriVox recordings are in the public domain.
