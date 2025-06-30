e37080220c6d0af37d3449a27ae9c64e2e6f11d5
Christoph Auer <cau@zurich.ibm.com>
2025-06-30 16:45:57 +0200

    feat: Introduce LayoutOptions to control layout postprocessing behaviour
    Signed-off-by: Christoph Auer <cau@zurich.ibm.com>

--- docling/models/layout_model.py
+++ docling/models/layout_model.py
@@ -13,6 +13,7 @@ from PIL import Image
 from docling.datamodel.accelerator_options import AcceleratorOptions
 from docling.datamodel.base_models import BoundingBox, Cluster, LayoutPrediction, Page
 from docling.datamodel.document import ConversionResult
+from docling.datamodel.pipeline_options import LayoutOptions
 from docling.datamodel.settings import settings
 from docling.models.base_model import BasePageModel
 from docling.models.utils.hf_model_download import download_hf_model
@@ -49,8 +50,12 @@ class LayoutModel(BasePageModel):
     CONTAINER_LABELS = [DocItemLabel.FORM, DocItemLabel.KEY_VALUE_REGION]
 
     def __init__(
-        self, artifacts_path: Optional[Path], accelerator_options: AcceleratorOptions
+        self,
+        artifacts_path: Optional[Path],
+        accelerator_options: AcceleratorOptions,
+        options: LayoutOptions,
     ):
+        self.options = options
         device = decide_device(accelerator_options.device)
 
         if artifacts_path is None:
@@ -176,7 +181,7 @@ class LayoutModel(BasePageModel):
                     # Apply postprocessing
 
                     processed_clusters, processed_cells = LayoutPostprocessor(
-                        page, clusters
+                        page, clusters, self.options
                     ).postprocess()
                     # Note: LayoutPostprocessor updates page.cells and page.parsed_page internally
 
