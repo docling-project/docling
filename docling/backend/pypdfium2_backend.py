cac15ad942654066017417f2777140b4fa4e94c1
Yussuf Shaikh <yussuf.shaikh1@ibm.com>
2026-01-12 19:47:13 +0530

    PdfImage get_pos renamed to get_bounds
    Signed-off-by: Yussuf Shaikh <yussuf.shaikh1@ibm.com>

--- docling/backend/pypdfium2_backend.py
+++ docling/backend/pypdfium2_backend.py
@@ -259,7 +259,10 @@ class PyPdfiumPageBackend(PdfPageBackend):
         with pypdfium2_lock:
             rotation = self._ppage.get_rotation()
             for obj in self._ppage.get_objects(filter=[pdfium_c.FPDF_PAGEOBJ_IMAGE]):
-                pos = obj.get_pos()
+                if hasattr(obj, "get_bounds"):
+                    pos = obj.get_bounds()   # pypdfium2 >= 5.x
+                else:
+                    pos = obj.get_pos()      # pypdfium2 <= 4.x
                 if rotation == 90:
                     pos = (
                         pos[1],
