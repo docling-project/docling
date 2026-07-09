a57836879afd1dc2e7c4dd64b0ecd73eb151b759
itsxenonli <bekas.nikolaoszug@gmail.com>
2026-07-09 10:40:26 +0200

    fix(video): use self.pipeline_options.enable_diarization
    Signed-off-by: itsxenonli <bekas.nikolaoszug@gmail.com>

--- docling/pipeline/video_pipeline.py
+++ docling/pipeline/video_pipeline.py
@@ -187,7 +187,7 @@ class VideoPipeline(BasePipeline):
                     transcript_items = self._asr_model.transcribe(wav_path)
                     transcript_items = merge_into_sentences(transcript_items)
                     # Run diarization while WAV is still available
-                    if opts.enable_diarization:
+                    if self.pipeline_options.enable_diarization:
                       try:
                         diarization = diarize(wav_path)
                         transcript_items = assign_speakers(
