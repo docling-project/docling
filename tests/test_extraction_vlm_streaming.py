3c0d4f7ea805f88c548daa58d4af9efeebef4000
Christoph Auer <cau@zurich.ibm.com>
2026-08-26 14:01:07 +0200

    Satisfy linter checks
    Signed-off-by: Christoph Auer <cau@zurich.ibm.com>

--- tests/test_extraction_vlm_streaming.py
+++ tests/test_extraction_vlm_streaming.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: The Docling Contributors
+# SPDX-License-Identifier: MIT
+
 import json
 from types import SimpleNamespace
 
--- tests/test_pdf_streaming_foundation.py
+++ tests/test_pdf_streaming_foundation.py
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: The Docling Contributors
+# SPDX-License-Identifier: MIT
+
 import itertools
 import time
 from types import MethodType, SimpleNamespace
